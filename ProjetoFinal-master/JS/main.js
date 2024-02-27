70e3e8cc094d699fed7b991b98be07900c45f54a
prof-lucassilva <106394691+prof-lucassilva@users.noreply.github.com>
2024-02-27 09:22:02 -0300

    Delete ProjetoFinal-master directory

--- ProjetoFinal-master/JS/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function() {
-    var menuMagico = document.getElementById('menuMagico');
-    var magia = window.scrollY > 50 ? "#edebd8" : "transparent"; // Azul se rolar mais de 50px
-
-    menuMagico.style.backgroundColor = magia;
-});
